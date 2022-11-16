9b4578850b32b1aae416f8b899195b2806d52c82
eryannaf <eryan.fajriani@gmail.com>
2022-11-16 21:56:40 +0700

    masterprice

--- database/migrations/2022_11_15_164951_create_master_prices_table.php
+++ database/migrations/2022_11_15_164951_create_master_prices_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMastersTable extends Migration
+class CreateMasterPricesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateMastersTable extends Migration
      */
     public function up()
     {
-        Schema::create('masters', function (Blueprint $table) {
+        Schema::create('master_prices', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->foreignId('user_id');
-            $table->string('day');
+            $table->decimal('price', 8, 2 );
         });
     }
 
@@ -28,6 +28,6 @@ class CreateMastersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('masters');
+        Schema::dropIfExists('master_prices');
     }
 }
